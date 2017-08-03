f412a75b8e93795ce178ece77e014f02ff7a624d
Eric Pascolo <eric.pascolo@cineca.it>
2017-08-03 17:25:14 +0200

    add and change version proprierty

--- checklib/__init__.py
+++ checklib/__init__.py
@@ -8,6 +8,6 @@ __author__ = "Eric Pascolo"
 __copyright__ = "Copyright 2017, B.U HPC - CINECA"
 __credits__ = [""]
 __license__ = "GNU Affero GPL v.3"
-__version__ = "0.1"
+__version__ = "0.1.001"
 __email__ = "eric.pascolo@cineca.it"
 __status__ = "Development"
