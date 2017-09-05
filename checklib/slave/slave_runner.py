2698b0e1eb978ddc13bfca27b4642ba793067d18
Eric Pascolo <eric.pascolo@cineca.it>
2017-09-05 14:31:26 +0200

    add multibenchmark analysis call

--- checklib/slave/slave_runner.py
+++ checklib/slave/slave_runner.py
@@ -4,10 +4,11 @@
 # @authors : Eric Pascolo
 #
 
+import traceback
 import logging
 import os
 from checklib.core import checkobj_result
-
+from checklib.slave import multibenchmark
 
 ####--------------------------------------------------------------------------------------------------------------
 
@@ -25,12 +26,14 @@ def main(check_core):
             check_results.append(cs.comparison())
         except:
             check_results.append(checkobj_result.check_result(cs.get_name(),"FAIL"))
-           
+            traceback.print_exc()
 
 
    
     for cr in check_results:
-        logger.critical(str(os.getenv("HOSTNAME")) +" --> "+str(cr.measure)+" "+cr.udm+" "+cr.check_status_dictionary[cr.status] )
+        logger.info(str(os.getenv("HOSTNAME")) +" --> "+str(cr.measure)+" "+cr.udm+" "+cr.status )
+
+    logger.critical(str(os.getenv("HOSTNAME")) +"  "+ multibenchmark.simple_mb_analisys(check_results)[0])
 
 ####--------------------------------------------------------------------------------------------------------------
     
