3426c9e31261622bd122b9f61cddedf50fea4c02
Eric Pascolo <eric.pascolo@cineca.it>
2020-03-02 17:46:50 +0100

    BF log flag and log management

--- checklib/inout/checklog.py
+++ checklib/inout/checklog.py
@@ -16,21 +16,24 @@ logger.addHandler(ch)
 ####--------------------------------------------------------------------------------------------------------------
 
 def checkloggin(loglevel,logfile,logtype="both"):
-    
 
+    if logfile != "__nofile__" and logtype == "cl":
+        logtype="both"
     # create logger with 'spam_application'
     logger_name = 'check_file_stream_log'
     logger = logging.getLogger(logger_name)
     logger.setLevel( loglevel  )
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('[%(levelname)s] %(asctime)s (%(module)s %(funcName)s) : %(message)s')
+
     # create file handler which logs even debug messages
-    fh = logging.FileHandler(logfile)
-    fh.setLevel(loglevel)
+    if logfile != "__nofile__":
+        fh = logging.FileHandler(logfile)
+        fh.setLevel(loglevel)
+        fh.setFormatter(formatter)
     # create console handler with a higher log level
     ch = logging.StreamHandler()
     ch.setLevel(loglevel)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('[%(levelname)s] %(asctime)s (%(module)s %(funcName)s) : %(message)s')
-    fh.setFormatter(formatter)
     ch.setFormatter(formatter)
     # add the handlers to the logger
 
@@ -42,9 +45,9 @@ def checkloggin(loglevel,logfile,logtype="both"):
         logger.addHandler(ch)
     if logtype == "file":
         logger.addHandler(fh)
-    
+
     logger.info("Enable logger name:"+logger_name+" type:"+logtype)
 
     return logger_name
-    
+
 ####--------------------------------------------------------------------------------------------------------------
