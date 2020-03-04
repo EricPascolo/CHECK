bbaa1ca395f9216b1c0eeade23663b305e2d673b
robertodavia <roberto.davia@gmail.com>
2020-03-04 18:14:39 +0100

    BF regex for hostlist

--- checklib/common/archive.py
+++ checklib/common/archive.py
@@ -1,10 +1,10 @@
 #
 # CHECK
 #
-# @authors : Eric Pascolo
+# @authors : Eric Pascolo, Roberto Da Vià
 #
 
 re_scientific_notation = '[\s=]+([+-]?(?:0|[1-9]\d*)(?:\.\d*)?(?:[eE][+\-]?\d+))$'
 re_path = '^(/)([^/\0]+(/)?)+$'
 re_splitsoftware = '[@#]'
-re_parser_hostlist='((([^\/^:\d]{1,})#([^:\d]{1,}))|([^:.]{1,})):([^:.]{1,})\/'
+re_parser_hostlist='(([^\/^:\d]{1,})#([^:\d]{1,}))|([^:.]{1,}):([^:.]{1,})([\/]|\n)'
