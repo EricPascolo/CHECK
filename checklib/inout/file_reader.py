3b35bd805a8d5763d8689ba5b897df6cbe064da8
Eric Pascolo <eric.pascolo@gmail.com>
2020-03-06 19:41:32 +0100

    add hpc_map_file_reader() , generic_file_reader()

--- checklib/inout/file_reader.py
+++ checklib/inout/file_reader.py
@@ -31,3 +31,44 @@ def json_reader(filepath):
     return filecontained
 
 ####--------------------------------------------------------------------------------------------------------------
+
+def hpc_map_file_reader(hpcmap_file):
+    """
+    Read a file that contains hpc map
+    Each row is composed by:
+
+        -name of the row
+        -array of string, comma separated
+    
+    The output is a dictionary of string array, the key is name of the row
+
+    """
+
+    hpcmap = {}
+
+    hpc_groups = generic_file_reader(hpcmap_file)
+
+    for group in hpc_groups:
+        group_name, group_array = group.split(" ")
+        group_nodes = group_array.split(",")
+        hpcmap.update({group_name: group_nodes}) 
+
+    return hpcmap
+
+####--------------------------------------------------------------------------------------------------------------
+
+def generic_file_reader(filename):
+    """
+        Read a generic ascii file and return an array of string (1 per row)
+    """
+    rows_from_file = []
+    if os.path.isfile(filename):
+        file_toread = open(filename, 'r')
+        rows_from_file = file_toread.readlines()
+        file_toread.close()
+    else:
+        print("ERROR file dosn't exist:"+filename)
+
+    return rows_from_file
+
+####--------------------------------------------------------------------------------------------------------------
