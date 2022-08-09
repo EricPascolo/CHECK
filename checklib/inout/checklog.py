a6ad7ad41357dabf9036c2c964121e12fa1bfee6
Giacomo Guiduzzi <guiduzzi.giacomo@gmail.com>
2022-08-09 11:35:33 +0200

    Reformats inout/checklog.py file, fixes a bug that printed an error in case the CL logger was set correctly. Edits the default CHECK config file to set the default log file to __nofile__ so to print only on command line (fixing a bug in the setting of the logger type).

--- checklib/inout/checklog.py
+++ checklib/inout/checklog.py
@@ -9,22 +9,24 @@ import logging
 
 logger_name = 'basic'
 logger = logging.getLogger(logger_name)
-logger.setLevel( "INFO"  )
+logger.setLevel("INFO")
 ch = logging.StreamHandler()
 logger.addHandler(ch)
 
 ####--------------------------------------------------------------------------------------------------------------
 
-def checkloggin(run_id,loglevel,logfile,logtype="both"):
+
+def checkloggin(run_id, loglevel, logfile, logtype="both"):
+    global logger_name, logger, ch
 
     if logfile != "__nofile__" and logtype == "cl":
-        logtype="both"
+        logtype = "both"
     # create logger with 'spam_application'
     logger_name = 'check_file_stream_log'
     logger = logging.getLogger(logger_name)
-    logger.setLevel( loglevel  )
+    logger.setLevel(loglevel)
     # create formatter and add it to the handlers
-    formatter = logging.Formatter(str(run_id)+' %(asctime)s [%(levelname)s] (%(funcName)s) : %(message)s',"%H:%M")
+    formatter = logging.Formatter(str(run_id)+' %(asctime)s [%(levelname)s] (%(funcName)s) : %(message)s', "%H:%M")
 
     # create file handler which logs even debug messages
     if logfile != "__nofile__":
@@ -37,27 +39,25 @@ def checkloggin(run_id,loglevel,logfile,logtype="both"):
     ch.setFormatter(formatter)
     # add the handlers to the logger
 
-
     if logtype == "both":
-
         logger.addHandler(ch)
+
         if logfile != "__nofile__":
             logger.addHandler(fh)
         else:
-            print("ERROR NO LOGGER FILE SET")
+            logger.critical("ERROR NO LOGGER FILE SET")
 
     elif logtype == "file":
-
         if logfile != "__nofile__":
             logger.addHandler(fh)
         else:
             print("ERROR NO LOGGER FILE SET")
     else:
-        logger.addHandler(ch)   #cl is default
-        if logtype != "both" or logtype == "cl" or logtype == "file":
-            print("ERROR WRONG LOG TYPE: cl is set by default")
+        logger.addHandler(ch)   # cl is default
+        if logtype not in ["both", "cl", "file"]:
+            logger.critical("ERROR WRONG LOG TYPE: cl is set by default")
 
-    logger.info("Logger type: "+logtype)
+    logger.info("Logger type: " + logtype)
 
     return logger_name
 
